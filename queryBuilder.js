1bc3d8dbd6288bf21ff63a565983affa7ad54fd7
mjugger <xaiox20@gmail.com>
2014-12-22 08:03:54 -0500

    expanded the constructor function

--- queryBuilder.js
+++ queryBuilder.js
@@ -5,16 +5,35 @@ var queryBuilder = (function(){
 		if(!(this instanceof constructor)) {
            throw 'queryBuilder must be constructed with new.';
         }
+
+        if(props){
+
+        	if(props.configFile){
+        		this.fetchJson(configFile);
+        		return true;
+        	}else{
+        		this.assignProps(props);
+        	}
+
+        }
+        this.assembler();
 	}
 
 	//method works in conjunction with the contructor to build app
-	constructor.prototype.assembler = function(){}
+	constructor.prototype.assembler = function(){
+		this.fetchJson();
+	}
 
-	//default proporties of this app
-	constructor.prototype.proporties = {}
+	//default properties of this app
+	constructor.prototype.properties = {}
+
+	//assigns new properties to the "properties" object.
+	constructor.prototype.assignProperties = function(){}
 
 	//retrieves json configuration file
-	constructor.prototype.fetchJson = function(){}
+	constructor.prototype.fetchJson = function(){
+		console.log('json called');
+	}
 
 	//exposes the constructor to the global environment
 	return constructor;
